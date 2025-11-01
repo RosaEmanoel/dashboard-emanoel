83419f749ad1ed4c2f20337dd6e015e0471f893d
RosaEmanoel <emanoelrosa0902@gmail.com>
2025-11-01 00:37:10 +0000

    ajuste

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -20,19 +20,16 @@ export function Login() {
                 <Card>
                     <Card.Body><Form>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                            <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                            </Form.Text>
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="Seu Email" />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Label>Senha</Form.Label>
+                            <Form.Control type="password" placeholder="Senha" />
                         </Form.Group>
                         <Button variant="primary" type="submit">
-                            Submit
+                            Entrar
                         </Button>
                     </Form>
                     </Card.Body>
